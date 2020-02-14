68e06ec83f87c65408f1742d7d7afda30d6aa684
Liz Kaufman <46270629+lizkaufman@users.noreply.github.com>
2020-02-14 16:26:46 +0000

    card generation nearly there; just spaced weird

--- createCard/createCard.js
+++ createCard/createCard.js
@@ -1,38 +1,42 @@
 // need to fetch from the backend API to get the data
-// can hopefully use innerHTML to generate most of the tags, with ${} to insert the different name and description from what the API returns
-// for types, because some have one and some have multiple, will need to use the JS to create li elements and then append them to the type ul
+// can hopefully use innerHTML to generate most of the tags, with ${} to insert the different bits
 
 function generateCard() {
-    
-    const response = await fetch('#'); // fetches the data from the API
-    const data = await response.json(); // JSONS the res into data
-    const pokemonCard = document.createElement('div').innerHTML = `
+  const name = 'testName';
+  const img_url =
+    'https://www.rd.com/wp-content/uploads/2019/11/cat-10-e1573844975155-1024x692.jpg';
+  const description = 'testDescript';
+  const types = ['exampleTypeOne', 'exampleTypeTwo'];
+
+  let typeList = '';
+  for (i = 0; i < types.length; i++) {
+    typeList += types[i] + ' ';
+  }
+  //const response = await fetch('#'); // fetches the data from the API
+  //const data = await response.json(); // JSONS the res into data
+  const pokemonCard = document.createElement('div');
+  pokemonCard.innerHTML = `
         <div class="pokemon-card">
             <p class="card-title">${name}</p>
-            <div class="pokemon-picture"><img src="${img} />></div>
+            <div class="pokemon-picture"><img src="${img_url} />></div>
             <div class="about-text-area">
                 <p class="label">Description:</p class="label">
-                <p class="description">${descrip}</p>
+                <p class="description">${description}</p>
                 <p class="label">Types:</p>
-                <ul class="types">
-                </ul>
+                <p class="types">${typeList}</p>
             </div>
         </div>
-    `; // creates the card itself with the easy bits filled in w/ ${}s
-    return 
-}
-
-//function to create the type list 
-function populateTypes(){
-    const typeList = ["water", "ground"]; // SAMPLE TEXT
-    for (i = 0; i<typeList.length; i++) {
-        document.body
-    }
+    `;
+  return pokemonCard;
 }
 
 //function to put the card on the page
 function appendCard() {
-    const newCard = generateCard();
-    const resultsArea = document.querySelector('.resultsArea');
-
+  const newCard = generateCard(); //runs generate card function
+  const resultsArea = document.querySelector('.results-area');
+  resultsArea.appendChild(newCard); // appends the new card generated above to the resultsArea div on the page
 }
+
+const testButton = document
+  .querySelector('#test-button')
+  .addEventListener('click', appendCard);
