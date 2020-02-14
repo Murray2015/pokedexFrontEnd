39782c54d1abd11913a1f8a9812d46d3c070b703
Liz Kaufman <46270629+lizkaufman@users.noreply.github.com>
2020-02-14 15:00:24 +0000

    planned out generate card function

--- createCard/createCard.js
+++ createCard/createCard.js
@@ -1,3 +1,5 @@
-function generateCard() {
-  document.createElement('div');
-}
+// need to fetch from the backend API to get the data
+// can hopefully use innerHTML to generate most of the tags, with ${} to insert the different name and description from what the API returns
+// for types, because some have one and some have multiple, will need to use the JS to create li elements and then append them to the type ul
+
+function generateCard() {}
