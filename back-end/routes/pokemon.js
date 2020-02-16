e4e6eeed7d59cfa784552480f8b7ff7647931338
Liz Kaufman <46270629+lizkaufman@users.noreply.github.com>
2020-02-16 21:46:24 +0000

    wrote out my logic for the search page in search.js along with explanatory comments; it doesn't actually work yet, but at least it's a start

--- back-end/routes/pokemon.js
+++ back-end/routes/pokemon.js
@@ -14,11 +14,7 @@ const {
 
 router.get('/pokemon', async (req, res) => {
   // Allows for using queries using any key-value pairs in the data; a query will be /pokemon?key=value in the path
-  const {
-    name,
-    search
-  } = req.query; // could also be const name = req.query.name
-  //const { searchTerm } = req.query;
+  const { name, search } = req.query;
   if (name) {
     // if name is specified, return that specific pokemon
     const namedPokemon = await getPokemonByName(name);
@@ -35,26 +31,19 @@ router.get('/pokemon', async (req, res) => {
 
 router.get('/pokemon/:pokemonId', async (req, res) => {
   // req.params takes in the id entered by the user in the path
-  const {
-    pokemonId
-  } = req.params; //what's after req.params has to match what's after the : in the path!
-  // same as req.params.pokemonID, but destructured instead of writing out
+  const { pokemonId } = req.params;
   const pokemon = await getPokemonById(pokemonId);
   res.json(pokemon);
 });
 
 router.post('/pokemon', async (req, res) => {
-  const {
-    body
-  } = req; // parse and extract the body and converted the JSON into an object
+  const { body } = req;
   await savePokemon(body);
   res.json(`You have saved ${body.name} as a pokemon.`);
 });
 
 router.delete('/pokemon/:pokemonId', async (req, res) => {
-  const {
-    pokemonId
-  } = req.params;
+  const { pokemonId } = req.params;
   const name = await deletePokemonById(pokemonId);
   if (name) {
     res.status(200).send(`Pokemon ${pokemonId}, ${name}, has been deleted.`);
@@ -64,12 +53,8 @@ router.delete('/pokemon/:pokemonId', async (req, res) => {
 });
 
 router.put('/pokemon/:pokemonId', async (req, res) => {
-  const {
-    body
-  } = req;
-  const {
-    pokemonId
-  } = req.params;
+  const { body } = req;
+  const { pokemonId } = req.params;
   const updatedPokemon = await updatePokemonById(pokemonId, body);
   if (updatedPokemon) {
     res.status(200).send(`You've updated pokemon ${pokemonId}`);
@@ -78,14 +63,9 @@ router.put('/pokemon/:pokemonId', async (req, res) => {
   }
 });
 
-
-router.patch('/pokemon/:pokemonId', async function (req, res) {
-  const {
-    body
-  } = req;
-  const {
-    pokemonId
-  } = req.params;
+router.patch('/pokemon/:pokemonId', async function(req, res) {
+  const { body } = req;
+  const { pokemonId } = req.params;
   const patchedPokemon = await patchPokemon(body, pokemonId);
   // res.json({
   //   success: true,
